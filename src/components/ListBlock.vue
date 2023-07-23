8fc8e43a0b6b598b5b0cedabe4295923c9fce948
Macovka <dmakkoveeva6@gmail.com>
2023-07-23 17:39:11 +0400

    shuffle

--- src/components/ListBlock.vue
+++ src/components/ListBlock.vue
@@ -2,23 +2,33 @@
   <div class="list-block">
     <div class="list-block__title-wrapper">
       {{ list.title }}
-      <button>Премешать</button>
-      <button>Премешать</button>
+      <button @click="shuffleSquares(commonArray)">Перемешать</button>
+      <button @click="sortSquares">Сортировать</button>
     </div>
     <div v-if="list.checkboxStatus === 'true' || 'part'">
       <div 
         v-for="item in list.items" 
         :key="item.id" 
       >
-      <div v-if="item.isChecked" class="list-block__squares">
+        <div v-if="item.isChecked">
+          <div v-if="!shuffled" class="list-block__squares">
+            <div
+              v-for="(square, index) in selectedSquares(item)" 
+              :key="index"
+              class="square" 
+              :style="{ backgroundColor: item.color }"
+            ></div>
+          </div>  
+        </div>
+      </div>
+      <div v-if="shuffled" class="list-block__squares">
         <div
-          v-for="(square, index) in selectedSquares(item)" 
-          :key="index"
+          v-for="item in commonArray" 
+          :key="item.color"
           class="square" 
           :style="{ backgroundColor: item.color }"
         ></div>
-      </div>
-      </div>
+      </div> 
     </div>
   </div>
 </template>
@@ -29,6 +39,11 @@
       list: {
         type: Object,
         required: true
+      }
+    },
+    data() {
+      return {
+        shuffled: false,
       }
     },
     computed: {
@@ -46,7 +61,36 @@
           return squares;
         };
       },
+      checkedItems() {
+        return this.list.items.filter((item) => item.isChecked);
+      },
+      commonArray() {
+        // Создание массив объектов, где каждый объект представляет элемент item,
+        // и количество объектов для каждого элемента равно его свойству quantity
+        const commonArray = [];
+        this.checkedItems.forEach((item) => {
+          const quantity = item.quantity;
+          for (let i = 0; i < quantity; i++) {
+            commonArray.push({ id: item.id, color: item.color });
+          }
+        });
+        return commonArray;
+      }
     },
+    methods: {
+      shuffleSquares(array) {     
+        // Тасование Фишера — Йетса
+        for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        
+        this.shuffled = true;
+      },
+      sortSquares() {
+        this.shuffled = false;
+      }
+    }
   }
 </script>
 
